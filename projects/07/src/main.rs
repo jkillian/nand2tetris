d37d543f8127a17d0b39b04f6e7510e00a6ec73a
Jason Killian <jason.killian@trialspark.com>
2021-01-04 00:34:03 -0500

    Vm translator part 2, memory access

--- projects/07/src/main.rs
+++ projects/07/src/main.rs
@@ -1,18 +1,35 @@
 use std::env;
 use std::fs;
 use std::io::{self, Read};
+use std::path::Path;
 
 use vmtranslator::parser;
 use vmtranslator::writer;
 
-fn get_files(path: Option<&str>) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+struct File {
+    content: String,
+    filename: String,
+}
+
+fn get_files(path: Option<&str>) -> Result<Vec<File>, Box<dyn std::error::Error>> {
     match path {
         None | Some("-") => {
             let mut data = String::new();
             io::stdin().read_to_string(&mut data)?;
-            Ok(vec![data])
+            Ok(vec![File {
+                content: data,
+                filename: "stdin".into(),
+            }])
         }
-        Some(file_path) if file_path.ends_with(".vm") => Ok(vec![fs::read_to_string(file_path)?]),
+        Some(file_path) if file_path.ends_with(".vm") => Ok(vec![File {
+            content: fs::read_to_string(file_path)?,
+            filename: Path::new(file_path)
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .into(),
+        }]),
         Some(dir_path) => {
             let results = fs::read_dir(dir_path)?
                 .filter_map(|entry| {
@@ -21,7 +38,10 @@ fn get_files(path: Option<&str>) -> Result<Vec<String>, Box<dyn std::error::Erro
                         Ok(entry) => entry.path(),
                     };
                     match file_path.extension() {
-                        Some(ext) if ext == "vm" => fs::read_to_string(file_path).ok(),
+                        Some(ext) if ext == "vm" => Some(File {
+                            content: fs::read_to_string(&file_path).unwrap(),
+                            filename: file_path.file_name().unwrap().to_str().unwrap().into(),
+                        }),
                         _ => None,
                     }
                 })
@@ -41,7 +61,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let files = get_files(args.get(1).map(|s| &s[..]))?;
     let mut writer = writer::CodeWriter::new();
     for file in files {
-        let commands = parser::parse_file_contents(&file[..])?;
+        let commands = parser::parse_file_contents(&file.content[..])?;
+
+        writer.set_filename(file.filename);
         for cmd in commands {
             println!("{}", writer.write_command(&cmd));
         }
--- projects/07/src/writer.rs
+++ projects/07/src/writer.rs
@@ -2,8 +2,7 @@ use crate::parser::{ArithmeticCommand, MemorySegment, VMCommand};
 
 #[derive(Default)]
 pub struct CodeWriter {
-    // TODO
-    // curr_filename: String,
+    curr_filename: String,
     unique_counter: usize,
 }
 
@@ -11,6 +10,12 @@ pub struct CodeWriter {
 const POP: &str = "@SP\nAM=M-1\nD=M";
 // Make M reference the value behind the current stack pointer value
 const LOOK_BACK: &str = "@SP\nA=M-1";
+
+const PUSH_D: &str = "@SP
+A=M
+M=D
+@SP
+M=M+1";
 
 fn comparison(comp: &str, lbl: &str) -> String {
     let commands: Vec<String> = vec![
@@ -27,13 +32,109 @@ fn comparison(comp: &str, lbl: &str) -> String {
     commands.join("\n")
 }
 
+fn memory_segment_pointer_reg(segment: &MemorySegment) -> String {
+    match segment {
+        MemorySegment::Local => "@LCL",
+        MemorySegment::Argument => "@ARG",
+        MemorySegment::This => "@THIS",
+        MemorySegment::That => "@THAT",
+        MemorySegment::Pointer => "@R3",
+        MemorySegment::Temp => "@R5",
+        _ => panic!("unhandled"),
+    }
+    .to_string()
+}
+
+fn memory_segment_direct_reg(segment: &MemorySegment) -> String {
+    match segment {
+        MemorySegment::Pointer => "@3",
+        MemorySegment::Temp => "@5",
+        _ => panic!("unhandled"),
+    }
+    .to_string()
+}
+
+fn memory_segment_pointer_push(segment: &MemorySegment, index: &usize) -> String {
+    let commands: Vec<String> = vec![
+        memory_segment_pointer_reg(segment),
+        "D=M".into(),
+        format!("@{}", index),
+        "A=A+D".into(),
+        "D=M".into(),
+        PUSH_D.into(),
+    ];
+    commands.join("\n")
+}
+
+fn memory_segment_direct_push(segment: &MemorySegment, index: &usize) -> String {
+    let commands: Vec<String> = vec![
+        memory_segment_direct_reg(segment),
+        "D=A".into(),
+        format!("@{}", index),
+        "A=A+D".into(),
+        "D=M".into(),
+        PUSH_D.into(),
+    ];
+    commands.join("\n")
+}
+
+fn memory_segment_static_push(filename: &str, index: &usize) -> String {
+    let commands: Vec<String> = vec![
+        format!("@{}.{}", filename, index),
+        "D=M".into(),
+        PUSH_D.into(),
+    ];
+    commands.join("\n")
+}
+
+fn memory_segment_pointer_pop(segment: &MemorySegment, index: &usize) -> String {
+    let commands: Vec<String> = vec![
+        memory_segment_pointer_reg(segment),
+        "D=M".into(),
+        format!("@{}", index),
+        "D=A+D".into(),
+        "@R13".into(),
+        "M=D".into(),
+        POP.into(),
+        "@R13".into(),
+        "A=M".into(),
+        "M=D".into(),
+    ];
+    commands.join("\n")
+}
+
+fn memory_segment_direct_pop(segment: &MemorySegment, index: &usize) -> String {
+    let commands: Vec<String> = vec![
+        memory_segment_direct_reg(segment),
+        "D=A".into(),
+        format!("@{}", index),
+        "D=A+D".into(),
+        "@R13".into(),
+        "M=D".into(),
+        POP.into(),
+        "@R13".into(),
+        "A=M".into(),
+        "M=D".into(),
+    ];
+    commands.join("\n")
+}
+
+fn memory_segment_static_pop(filename: &str, index: &usize) -> String {
+    let commands: Vec<String> = vec![POP.into(), format!("@{}.{}", filename, index), "M=D".into()];
+    commands.join("\n")
+}
+
 impl CodeWriter {
     pub fn new() -> CodeWriter {
-        CodeWriter { unique_counter: 0 }
+        CodeWriter {
+            unique_counter: 0,
+            curr_filename: String::new(),
+        }
     }
 
-    // TODO
-    pub fn set_file_name(&self) {}
+    pub fn set_filename(&mut self, filename: String) {
+        self.curr_filename = filename.trim_end_matches(".vm").into();
+    }
 
     pub fn write_command(&mut self, command: &VMCommand) -> String {
         match command {
@@ -61,18 +162,29 @@ impl CodeWriter {
                 ArithmeticCommand::Not => vec![LOOK_BACK, "M=!M"].join("\n"),
             },
             VMCommand::PushCommand(memory_segment, index) => match memory_segment {
-                MemorySegment::Constant => vec![
-                  format!("@{}", index),
-                  "D=A".into(),
-                  "@SP".into(),
-                  "A=M".into(),
-                  "M=D".into(),
-                  "@SP".into(),
-                  "M=M+1".into(),
-                ].join("\n"),
-                _ => format!("TODO"),
+                MemorySegment::Constant => {
+                    vec![format!("@{}", index), "D=A".into(), PUSH_D.into()].join("\n")
+                }
+                MemorySegment::Local
+                | MemorySegment::Argument
+                | MemorySegment::This
+                | MemorySegment::That => memory_segment_pointer_push(memory_segment, index),
+                MemorySegment::Pointer | MemorySegment::Temp => {
+                    memory_segment_direct_push(memory_segment, index)
+                }
+                MemorySegment::Static => memory_segment_static_push(&self.curr_filename[..], index),
             },
-            VMCommand::PopCommand(_memory_segment, _index) => format!("pop"),
+            VMCommand::PopCommand(memory_segment, index) => match memory_segment {
+                MemorySegment::Constant => panic!("Cannot pop to contant"),
+                MemorySegment::Local
+                | MemorySegment::Argument
+                | MemorySegment::This
+                | MemorySegment::That => memory_segment_pointer_pop(memory_segment, index),
+                MemorySegment::Pointer | MemorySegment::Temp => {
+                    memory_segment_direct_pop(memory_segment, index)
+                }
+                MemorySegment::Static => memory_segment_static_pop(&self.curr_filename[..], index),
+            },
         }
     }
 }
